58b3b328ed02920642212aab4d103afbb3dc64aa
Shivam Kumar <shivamstm01@gmail.com>
2024-03-07 11:50:46 +0530

    "changes done"

--- src/app/course/course/course.component.ts
+++ src/app/course/course/course.component.ts
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-  VideoPath=['pexels_videos_4409 (1080p).mp4','human_feeding_the_little_squirrel (Original).mp4'];
+  VideoPaths = ['pexels_videos_4409 (1080p).mp4', 'human_feeding_the_little_squirrel (Original).mp4'];
 
+  videos = [
+    { path: 'pexels_videos_4409 (1080p).mp4', description: 'Description for video 1' },
+    { path: 'human_feeding_the_little_squirrel (Original).mp4', description: 'Description for video 2' },
+    { path: 'pexels_videos_4409 (1080p).mp4', description: 'Description for video 3' },
+    { path: 'human_feeding_the_little_squirrel (Original).mp4', description: 'Description for video 4' },
+    { path: 'pexels_videos_4409 (1080p).mp4', description: 'Description for video 5' },
+    { path: 'human_feeding_the_little_squirrel (Original).mp4', description: 'Description for video 5' },
+    { path: 'pexels_videos_4409 (1080p).mp4', description: 'Description for video 3' },
+    { path: 'human_feeding_the_little_squirrel (Original).mp4', description: 'Description for video 6' }
+];
+
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+  getVideoDescription(videoPath: string): string {
+    switch (videoPath) {
+        case 'pexels_videos_4409 (1080p).mp4':
+            return 'Description for video 1';
+        case 'human_feeding_the_little_squirrel (Original).mp4':
+            return 'Description for video 2';
+        default:
+            return 'No description available';
+    }
+}
  
 }
